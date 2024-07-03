8de355b7098ccda2fcbfd5c04e4ef7e425b7d10b
claudiarochadop <claudiarochadop@gmail.com>
2024-07-03 16:34:11 -0500

    agrego funciones de botones de header y funcion en input url para agregar la imagen

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
-/*---Botones Header---*/
+/* -----------------------CAPTURA DE ELEMENTOS ---------------------*/
 
-let botonTexto = document.getElementById("boton-texto");
-let botonImagen = document.getElementById("boton-imagen");
-let botonModoOscuro = document.getElementById("boton-modo-oscuro");
-let seccionTexto = document.getElementById("seccion-texto");
-let seccionImagen = document.getElementById("seccion-imagen");
-let claseOculto = document.querySelector(".oculto");
+/*---Botones Header y Captura de Secciones---*/
+
+const botonTexto = document.getElementById("boton-texto");
+const botonImagen = document.getElementById("boton-imagen");
+const botonModoOscuro = document.getElementById("boton-modo-oscuro");
+const seccionTexto = document.getElementById("seccion-texto");
+const seccionImagen = document.getElementById("seccion-imagen");
+const claseOculto = document.querySelector(".oculto");
+const headerTag = document.getElementsByTagName("header");
+const modoOscuro = document.querySelector(".modo-oscuro");
+const modoClaro = document.querySelector(".modo-claro");
+
+/*---  Elementos input URL  ---*/
+
+const inputUrl = document.getElementById("input-url");
+const imagenMeme = document.getElementById("img-meme");
+
+/*---  Elementos Color  ---*/
+
+const 
+
+/* -----------------------  FUNCIONES ---------------------*/
+
+/* ----  Funciones Botones Header ------ */
 
 botonTexto.addEventListener("click", function () {
   seccionTexto.style.display = "block";
@@ -17,4 +35,16 @@ botonImagen.addEventListener("click", function () {
   seccionImagen.style.display = "block";
 });
 
-// // botonModoOscuro =
+botonModoOscuro.addEventListener("click", function () {
+  // modoOscuro.classList.toggle(".modo-claro");
+  // modoClaro.classList.toggle(".modo-oscuro");
+  // // modoOscuro.classList.replace(".modo-oscuro", ".modo-claro");
+  modoOscuro.classList.toggle("modo-claro");
+  modoOscuro.classList.toggle("modo-oscuro");
+});
+
+/* ----  Funciones Formulario Imagen ------ */
+
+inputUrl.oninput = () => {
+  imagenMeme.src = inputUrl.value;
+};
